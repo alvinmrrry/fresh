11c4fc7b4feabecb1f93e5f4fe16328bc51fc260
alvinmrrry <alvin_mrrry@outlook.com>
2025-05-07 14:46:21 +0000

    May 7, 2025, 10:46 PM

--- routes/blog/index.tsx
+++ routes/blog/index.tsx
@@ -2,44 +2,44 @@ import { Handlers } from "$fresh/server.ts";
 
 const CategoryData = [
     {
-        name:"category1",
-        status:1,
-        date:Date.now(),
+        name: "category1",
+        status: 1,
+        date: Date.now(),
     },
     {
-        name:"category2",
-        status:1,
-        date:Date.now(),
+        name: "category2",
+        status: 1,
+        date: Date.now(),
     },
     {
-        name:"category3",
-        status:1,
-        date:Date.now(),
+        name: "category3",
+        status: 1,
+        date: Date.now(),
     },
     {
-        name:"category4",
-        status:1,
-        date:Date.now(),
+        name: "category4",
+        status: 1,
+        date: Date.now(),
     },
     {
-        name:"category5",
-        status:1,
-        date:Date.now(),
+        name: "category5",
+        status: 1,
+        date: Date.now(),
     },
     {
-        name:"category6",
-        status:1,
-        date:Date.now(),
+        name: "category6",
+        status: 1,
+        date: Date.now(),
     },
     {
-        name:"category7",
-        status:1,
-        date:Date.now(),
+        name: "category7",
+        status: 1,
+        date: Date.now(),
     },
 ]
 
 export const handler: Handlers = {
-    async GET(_req,ctx:any) {
+    async GET(_req, ctx: any) {
         try {
             const categories = CategoryData;
             return await ctx.render(categories);
@@ -51,9 +51,9 @@ export const handler: Handlers = {
 
 export default function blog() {
     return (
-       <div>
-        this is blog page!
-       </div>
+        <div>
+            this is blog page!
+        </div>
     );
 }
 
