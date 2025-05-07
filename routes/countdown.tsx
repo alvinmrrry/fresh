c23ee29e85975819e8ae163bac53b031311f38e9
alvinmrrry <alvin_mrrry@outlook.com>
2025-05-07 13:44:43 +0000

    May 7, 2025, 9:44 PM

--- routes/countdown.tsx
+++ routes/countdown.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     date.setHours(date.getHours() + 1);
     return (
         <div class="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-            The big event is happening <Countdown target={date.toISOString()} />.
+            The big event is happening  <Countdown target={date.toISOString()} />.
         </div>
     );
 }
